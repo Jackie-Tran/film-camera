d70956830a616717f10bf9d577951d4833650ff0
Jackie <jtran2009a@gmail.com>
2022-10-27 02:45:07 -0400

    use image crate to read image

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-pub mod image;
-
+use ::image::io::Reader as ImageReader;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 
-fn get_file_contents(buffer: &mut Vec<u8>, file_path: &String) {
-    let mut file = File::open(file_path).expect("error");
-    // TODO: use buffer and multithreading to process file contents in case of large files
-    file.read_to_end(buffer).expect("Error reading file");
-    println!("Read {} bytes", buffer.len());
-}
-
-fn decode_jpeg() {}
+fn create_gaussian_noise() {}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let mut file_buffer: Vec<u8> = Vec::new();
-    get_file_contents(&mut file_buffer, file_path);
-
-    image::decode_png(&file_buffer);
+    let img = ImageReader::open(file_path).expect("valid image file to exist");
+    let mut img = img.decode().expect("decode image");
 }
