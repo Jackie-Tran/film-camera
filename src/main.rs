279fdc368802e65fc2042eceb292c82e199741e4
Jackie <jtran2009a@gmail.com>
2022-10-28 01:46:38 -0400

    feat: added grayscale noise

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,46 @@ use rand;
 use rand_distr::{Distribution, Normal};
 use std::env;
 
-fn create_gaussian_noise(mean: f64, sd: f64, width: u32, height: u32) -> Vec<u8> {
+fn create_gaussian_noise(mean: f64, sd: f64, width: u32, height: u32, grayscale: bool) -> Vec<i16> {
     let normal = Normal::new(mean, sd).unwrap();
     let mut rng = rand::thread_rng();
     let mut gaussian = normal.sample_iter(&mut rng);
-    let mut noise = Vec::<u8>::new();
+    let mut noise = Vec::<i16>::new();
     while noise.len() < (width * height * 4) as usize {
-        for i in (0..3) {
-            let val = (gaussian.next().unwrap() * 255.0) as u8;
-            noise.push(val);
+        match grayscale {
+            true => {
+                let val = (gaussian.next().unwrap() * 255.0) as i16;
+                noise.extend([val, val, val, 255])
+            }
+            _ => {
+                for _ in 0..3 {
+                    let val = (gaussian.next().unwrap() * 255.0) as i16;
+                    noise.push(val);
+                }
+                noise.push(255);
+            }
         }
-        noise.push(255);
     }
     return noise;
 }
 
-fn add_two_images(image1: &[u8], image2: &[u8]) -> Vec<u8> {
-    assert!(image1.len() == image2.len());
+fn add_noise(image: &[u8], noise: &[i16]) -> Vec<u8> {
+    assert!(image.len() == noise.len());
     let mut sum_image = Vec::<u8>::new();
-    for i in 0..image1.len() {
-        if image1[i] > std::u8::MAX - image2[i] {
-            sum_image.push(255);
-        } else if image1[i] < image2[i] {
+    for i in 0..image.len() {
+        if noise[i] > 0 {
+            // We are adding
+            if image[i] > 255 - noise[i] as u8 {
+                sum_image.push(255);
+            } else {
+                sum_image.push(image[i] + noise[i] as u8);
+            }
+        } else if image[i] < -noise[i] as u8 {
+            // We underflow
             sum_image.push(0);
         } else {
-            sum_image.push(image1[i] + image2[i]);
+            // We are subtracting
+            sum_image.push(image[i] - -noise[i] as u8);
         }
     }
     return sum_image;
@@ -40,16 +55,9 @@ fn main() {
     let img = ImageReader::open(file_path).expect("valid image file to exist");
     let img = img.decode().expect("decode image");
     println!("{:?} {} {} ", img.color(), img.width(), img.height());
-    let noise = create_gaussian_noise(0.0, 0.2, img.width(), img.height());
-    let _ = image::save_buffer(
-        "./noise.png",
-        &noise[0..],
-        img.width(),
-        img.height(),
-        image::ColorType::Rgba8,
-    );
+    let noise = create_gaussian_noise(0.0, 0.08, img.width(), img.height(), true);
     // Apply noise
-    let sum_image = add_two_images(img.as_bytes(), &noise);
+    let sum_image = add_noise(img.as_bytes(), &noise);
     println!("{:?}", &sum_image[0..5]);
     let _ = image::save_buffer(
         "./noisy_image.png",
@@ -59,5 +67,5 @@ fn main() {
         image::ColorType::Rgba8,
     );
     println!("{} {:?}", img.as_bytes().len(), &img.as_bytes()[0..5]);
-    println!("{} {:?}", noise.len(), &noise[0..5]);
+    // println!("{} {:?}", noise.len(), &noise);
 }
