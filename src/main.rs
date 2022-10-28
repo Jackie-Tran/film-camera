de0f3e78271323f3300f0382f85180570b4ed60f
Jackie <jtran2009a@gmail.com>
2022-10-27 22:45:04 -0400

    feat: add noise to image

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,29 @@ fn create_gaussian_noise(mean: f64, sd: f64, width: u32, height: u32) -> Vec<u8>
     let mut rng = rand::thread_rng();
     let mut gaussian = normal.sample_iter(&mut rng);
     let mut noise = Vec::<u8>::new();
-    while noise.len() < (width * height) as usize {
-        let val = (gaussian.next().unwrap().abs() * 255.0) as u8;
-        noise.push(val);
+    while noise.len() < (width * height * 4) as usize {
+        for i in (0..3) {
+            let val = (gaussian.next().unwrap() * 255.0) as u8;
+            noise.push(val);
+        }
+        noise.push(255);
     }
     return noise;
+}
+
+fn add_two_images(image1: &[u8], image2: &[u8]) -> Vec<u8> {
+    assert!(image1.len() == image2.len());
+    let mut sum_image = Vec::<u8>::new();
+    for i in 0..image1.len() {
+        if image1[i] > std::u8::MAX - image2[i] {
+            sum_image.push(255);
+        } else if image1[i] < image2[i] {
+            sum_image.push(0);
+        } else {
+            sum_image.push(image1[i] + image2[i]);
+        }
+    }
+    return sum_image;
 }
 
 fn main() {
@@ -22,13 +40,23 @@ fn main() {
     let img = ImageReader::open(file_path).expect("valid image file to exist");
     let img = img.decode().expect("decode image");
     println!("{:?} {} {} ", img.color(), img.width(), img.height());
-    let noise = create_gaussian_noise(0.0, 0.1, img.width(), img.height());
+    let noise = create_gaussian_noise(0.0, 0.2, img.width(), img.height());
     let _ = image::save_buffer(
         "./noise.png",
         &noise[0..],
         img.width(),
         img.height(),
-        image::ColorType::L8,
+        image::ColorType::Rgba8,
+    );
+    // Apply noise
+    let sum_image = add_two_images(img.as_bytes(), &noise);
+    println!("{:?}", &sum_image[0..5]);
+    let _ = image::save_buffer(
+        "./noisy_image.png",
+        &sum_image[0..],
+        img.width(),
+        img.height(),
+        image::ColorType::Rgba8,
     );
     println!("{} {:?}", img.as_bytes().len(), &img.as_bytes()[0..5]);
     println!("{} {:?}", noise.len(), &noise[0..5]);
